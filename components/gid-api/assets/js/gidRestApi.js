cdf43e07c84395fda779543c738914a077b46fb4
Shreya Bharadwaj <shreyab@nrifintech.com>
2017-08-28 17:33:12 +0530

    Fetched latest changes of components

--- components/gid-api/assets/js/gidRestApi.js
+++ components/gid-api/assets/js/gidRestApi.js
@@ -1,19 +1,60 @@
 var apiPath = {
-		"lobs": "/lobs",
-	    "concepts": "/lobs/{lob}/domains",
-	    "applications": "/domains/{concept}/applications",
-	    "databases": "/domains/{concept}/applications/{application}/databases",
-	    "schemas": "/domains/{concept}/applications/{application}/databases/{database}/schemas",
-	    "tables": "/domains/{concept}/schemas/{schema}/tables",
-	    "columns": "/domains/{concept}/tables/{table}/columns",         
-	    "get-samples": "/samples",
-	    "recommended-concepts": "/columns/{column}/domains",
-	    "verify-concept": "/columns/{column}/domains/{concept}",
-	    "list-concepts": "/domains",
-	    "concept-details": "/domains/{concept}",
-	    "curate-concept": "/domains/{concept}/curation",
-	    "create-concept": "/domains",
-	    "edit-concept": "/domains/{concept}",
-	    "remove-concept": "/domains/{concept}",
-	    "auth": "/api/security/api-token-v1"
-	};
+		  "lobs":{
+			  "default": "/lobs" 
+		  },
+		  "concepts": {
+			"default": "/lobs/{lob}/domains"
+		  },
+		  "applications": {
+		    "default": "/domains/{concept}/applications",
+		    "all": "/applications"
+		  },
+		  "databases": {
+		    "default": "/domains/{concept}/applications/{application}/databases",
+		    "all": "/applications/{application}/databases"
+		  },
+		  "schemas": {
+		    "default": "/domains/{concept}/applications/{application}/databases/{database}/schemas",
+		    "all": "/applications/{application}/databases/{database}/schemas"
+		  },
+		  "tables": {
+		    "default": "/domains/{concept}/schemas/{schema}/tables",
+		    "all": "/schemas/{schema}/tables"
+		  },
+		  "columns": {
+		    "table": "/domains/{concept}/tables/{table}/columns",
+		    "schema": "/domains/{concept}/schemas/{schema}/columns",
+		    "database": "/domains/{concept}/applications/{application}/databases/{database}/columns",
+		    "application": "/domains/{concept}/applications/{application}/columns"
+		  },
+		  "get-samples": {
+			 "default": "/samples"
+		  },
+		  "recommended-concepts": {
+			 "default": "/columns/{column}/domains"
+		  },
+		  "verify-concept": {
+			  "default": "/columns/{column}/domains/{concept}"
+		  },
+		  "list-concepts": {
+			  "default": "/domains"
+		  },
+		  "concept-details":{
+			  "default": "/domains/{concept}"
+		  },
+		  "curate-concept": {
+			  "default": "/domains/{concept}/curation"
+		  },
+		  "create-concept": {
+			  "default": "/domains"
+		  },
+		  "edit-concept": {
+			  "default": "/domains/{concept}"
+		  },
+		  "remove-concept": {
+			  "default": "/domains/{concept}"
+		  },
+		  "auth": {
+			  "default": "/api/security/api-token-v1"
+		  }
+		};
